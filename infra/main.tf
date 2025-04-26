a728ba894a108dc06cd8a5b981d3b4f7995e6921
raghavsikaria <raghavsikaria9@gmail.com>
2025-04-25 22:16:11 -0400

    Adding register_timestamp for user, and APIT GATEWAY ratelimiting2/n

--- infra/main.tf
+++ infra/main.tf
@@ -178,7 +178,7 @@ resource "aws_apigatewayv2_stage" "default" {
   api_id      = aws_apigatewayv2_api.http_api.id
   name        = "$default"
   auto_deploy = true
-  
+
   default_route_settings {
     throttling_burst_limit = 10
     throttling_rate_limit  = 5
